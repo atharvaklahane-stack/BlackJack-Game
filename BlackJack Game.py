bf59b3b4b14908155c8979da86d63da9bd2ca05f
atharvaklahane-stack <atharvaklahane@gmail.com>
2026-02-22 22:23:12 +0530

    Refactor Blackjack game logic and improve user prompts

--- BlackJack Game.py
+++ BlackJack Game.py
@@ -7,7 +7,7 @@ def blackjack():
         c1.append(random.choice(nos))
         c2.append(random.choice(nos))
     s1=sum(c1)
-    s2=sum(c2)
+    s2=(sum(c2))
     print(f"MY CARDS: {c1}| MY SCORE: {s1}")
     print(f"DEALER'S 1ST CARD: {c2[0]}")
     if s1==21 and len(c1)==2:
@@ -17,37 +17,30 @@ def blackjack():
         print('YOU LOSE!!\nTHE DEALER GOT THE BLACKJACK ')
         return
     
-    a=input("DO YOU WANT TO DRAW A CARD!!(Y/N)")
+    a=input("DO YOU WANT TO DRAW A CARD!!(Y/N)").upper()
     for i in range(1):
         if a =='Y':
             c1.append(random.choice(nos))
             s1=sum(c1)
-            print(c1)
-            print(s1)
-            if ((s1>21 and s2>21) and(s1==s2)):
-                print("BUST!!")
-                print(f"MY CARDS: {c1}| MY SCORE: {s1}")
-                print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
+            
             if s1>21:
                 if 11 in c1:
-                    c1.append(random.choice(nos))
-                    s1=sum(c1)
                     c1[c1.index(11)]=1
                     s1=sum(c1)
-                    print("YOU LOSE!!")
-                    print(f"MY CARDS: {c1}| MY SCORE: {s1}")
-                    print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
+                    if s1>s2:
+                        print("YOU WIN!!")
+                        print(f"MY CARDS: {c1}| MY SCORE: {s1}")
+                        print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
                 else:
-                    print("YOU LOSE!!")
+                    print("YOU LOSE!!\nYOUR SCORE IS OVER 21!")
                     print(f"MY CARDS: {c1}| MY SCORE: {s1}")
                     print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
-            elif s1<22 and s2<22:
-                if  s1<s2:
-                    print("YOU LOSE!!")
-                    print(f"MY CARDS: {c1}| MY SCORE: {s1}")
-                    print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
-                elif s2<s1:
+            elif s1>s2:
                     print("YOU WIN!!")
+                    print(f"MY CARDS: {c1}| MY SCORE: {s1}")
+                    print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
+            elif s2>s1:
+                    print("YOU LOSE!!")
                     print(f"MY CARDS: {c1}| MY SCORE: {s1}")
                     print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
         else:
@@ -57,7 +50,12 @@ def blackjack():
                 if s2>21 and 11 in c2:
                     c2[c2.index(11)]=1
                     s2=sum(c2)
-            if s2>21:
+            if s2>21 and 11 in c2:
+                    c2[c2.index(11)]=1
+                    s2=sum(c2)
+                    print(f"MY CARDS: {c1}| MY SCORE: {s1}")
+                    print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
+            elif s2>21:
                 print("YOU WIN!!\n THE DEALER'S SCORE IS OVER 21!")
                 print(f"MY CARDS: {c1}| MY SCORE: {s1}")
                 print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
@@ -74,13 +72,13 @@ def blackjack():
                     print("PUSH!!")
                     print(f"MY CARDS: {c1}| MY SCORE: {s1}")
                     print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
-        s2=sum(c2)
-        if s2>21 and 11 in c2:
-                c2[c2.index(11)]=1
-                s2=sum(c2)
-                print(f"MY CARDS: {c1}| MY SCORE: {s1}")
-                print(f"DEALER'S CARDS: {c2}| DEALER'S SCORE: {s2}")
-while input('''    ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ 
-    DO YOU WANT TO PLAY A GAME OF BLACKJACK? (y/n): ''') == 'Y':
-    print("    ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠")
-    blackjack()
+while True:
+    play = input('''    ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ ♠ ♥ ♦ ♣ 
+    DO YOU WANT TO PLAY A GAME OF BLACKJACK? (y/n): ''').upper()
+    
+    if play == 'Y':
+        print("    ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠ ♣ ♦ ♥ ♠")
+        blackjack()
+    else:
+        print("Thanks for playing! Goodbye ♠ ♥ ♦ ♣")
+        break
